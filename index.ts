0847b9e5132ef9ce31e8d9e19301f76617e84194
MUHAMMAD-HUSNAIN-GHORI <160330174+MUHAMMAD-HUSNAIN-GHORI@users.noreply.github.com>
2024-05-03 16:20:58 +0500

    created Number guessing Game to win ticket

---
+++ index.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from 'chalk';
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+  {
+    name: "HusnainGhori",
+    type: "input",
+    message: chalk.green("The user is requested to select any number from 1 to 10. If any number matches your number, we will give you Umrah retuned ticket."),
+    validate: (name) => {
+      if (name === "") {
+        return (chalk.red('Error!U Need To Add A Valid Number'))        
+      } else {
+        return true
+      }
+    }
+  }
+])
+if (answer.HusnainGhori===randomNumber){
+     console.log("congratulations Umrah tikets")}
+     else {console.log(chalk.yellowBright("\n\nbest of luck to next time"))}
+
+
+
+
+
+
+
